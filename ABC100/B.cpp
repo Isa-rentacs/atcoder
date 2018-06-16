86fee168a3350630107af90a75a797726756f3cf
Junnosuke Kuroda <jkuroda.isa@gmail.com>
2018-06-16 22:47:46 +0900

    Add ABC100 A/B/C

---
+++ ABC100/B.cpp
@@ -0,0 +1,42 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <cctype>
+#include <string>
+#include <cstring>
+#include <ctime>
+
+using namespace std;
+
+#define dump(x)  cerr << #x << " = " << (x) << endl;
+#define PB push_back
+#define MP make_pair
+#define ll long long
+
+inline int toInt(string s){int v;istringstream sin(s);sin>>v;return v;}
+template<class T> inline string toString(T x){ostringstream sout;sout<<x;return sout.str();}
+
+int main(){
+    std::ios::sync_with_stdio(false);
+    int d, n;
+    cin >> d >> n;
+
+    int ans = pow(100, d) * (n == 100 ? 101 : n);
+
+    cout << ans << endl;
+}
